38f2e12b66b0e6f8fb61a603d786df800e0b4f0e
Matias <mantitaaa89@gmail.com>
2023-09-20 16:51:33 -0300

    first commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -11,10 +13,10 @@ const config: HardhatUserConfig = {
     },
     goerli: {
       url: process.env.GOERLI_RPC_URL,
-      accounts:{
-        mnemonic: process.env.PRIVATE_KEY,
-      }
-    }
+      // accounts: {
+      //   mnemonic: process.env.PRIVATE_KEY,
+      // },
+    },
   },
 };
 
--- scripts/createMimeToken.ts
+++ scripts/createMimeToken.ts
@@ -1,5 +1,7 @@
 import { ethers } from "hardhat";
 import OsmoticController from "../abis/OsmoticController.json";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const OSMOTIC_CONTROLLER_ADDRESS = "0x0b9f52138050881C4d061e6A92f72d8851B59F8e";
 const PROJECT_REGISTRY_ADDRESS = "0xFb5Ff528E295a39b1ba0b053FF7cA410396932c0";
@@ -44,30 +46,30 @@ async function main() {
 
   console.log("MimeToken", mimeToken);
 
-  // create osmotic pool
-  const openList = PROJECT_REGISTRY_ADDRESS;
+  // // create osmotic pool
+  // const openList = PROJECT_REGISTRY_ADDRESS;
 
-  const poolInitCode = new ethers.utils.Interface(
-    OSMOTIC_POOL_ABI
-  ).encodeFunctionData("initialize", [
-    FUNDING_TOKEN,
-    mimeToken,
-    openList,
-    [
-      '999999197747000000', // 10 days (864000 seconds) to reach 50% of targetRate
-      2,
-      19290123456, // 5% of Common Pool per month = Math.floor(0.05e18 / (30 * 24 * 60 * 60))
-      '25000000000000000', // 2.5% of Total Support = the minimum stake to start receiving funds
-    ],
-  ]);
+  // const poolInitCode = new ethers.utils.Interface(
+  //   OSMOTIC_POOL_ABI
+  // ).encodeFunctionData("initialize", [
+  //   FUNDING_TOKEN,
+  //   mimeToken,
+  //   openList,
+  //   [
+  //     '999999197747000000', // 10 days (864000 seconds) to reach 50% of targetRate
+  //     2,
+  //     19290123456, // 5% of Common Pool per month = Math.floor(0.05e18 / (30 * 24 * 60 * 60))
+  //     '25000000000000000', // 2.5% of Total Support = the minimum stake to start receiving funds
+  //   ],
+  // ]);
 
-  const tx2 = await controller.createOsmoticPool(poolInitCode);
-  const { events: createOsmoticPoolEvents } = await tx2.wait();
-  const { pool } = createOsmoticPoolEvents.find(
-    ({ event }: { event: string }) => event === "OsmoticPoolCreated"
-  ).args;
+  // const tx2 = await controller.createOsmoticPool(poolInitCode);
+  // const { events: createOsmoticPoolEvents } = await tx2.wait();
+  // const { pool } = createOsmoticPoolEvents.find(
+  //   ({ event }: { event: string }) => event === "OsmoticPoolCreated"
+  // ).args;
 
-  console.log("OsmoticPool", pool);
+  //console.log("OsmoticPool", pool);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
